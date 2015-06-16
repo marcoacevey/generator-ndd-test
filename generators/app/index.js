1189aa2ba1a6a66b6a2efaea3d713f978a0a046f
marcoacevey <marco.cevey@outlook.com>
2015-06-16 14:12:42 -0300

    version1

--- generators/app/index.js
+++ generators/app/index.js
@@ -4,54 +4,62 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 module.exports = yeoman.generators.Base.extend({
-  prompting: function () {
-    var done = this.async();
+    prompting: function () {
+        var done = this.async();
 
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the first-rate ' + chalk.red('NddTest') + ' generator!'
-    ));
+        // Have Yeoman greet the user.
+        this.log(yosay(
+          'Welcome to the first-rate ' + chalk.red('NddTest') + ' generator!'
+        ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+        var prompts = [{
+            type: 'confirm',
+            name: 'someOption',
+            message: 'Would you like to enable this option?',
+            default: true
+        }];
 
-    this.prompt(prompts, function (props) {
-      this.props = props;
-      // To access props later use this.props.someOption;
+        this.prompt(prompts, function (props) {
+            this.props = props;
+            // To access props later use this.props.someOption;
 
-      done();
-    }.bind(this));
-  },
-
-  writing: {
-    app: function () {
-      this.fs.copy(
-        this.templatePath('_package.json'),
-        this.destinationPath('package.json')
-      );
-      this.fs.copy(
-        this.templatePath('_bower.json'),
-        this.destinationPath('bower.json')
-      );
+            done();
+        }.bind(this));
     },
 
-    projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
-    }
-  },
+    writing: {
+        app: function () {
+            this.fs.copy(
+              this.templatePath('_package.json'),
+              this.destinationPath('package.json')
+            );
+            this.fs.copy(
+              this.templatePath('_bower.json'),
+              this.destinationPath('bower.json')
+            );
+            this.fs.copy(
+                this.templatePath('_bug.jpg'),
+                this.destinationPath('bug.jpg')
+            );
+            this.fs.copy(
+                this.templatePath('_index.html'),
+                this.destinationPath('index.html')
+            );
+        },
 
-  install: function () {
-    this.installDependencies();
-  }
+        projectfiles: function () {
+            this.fs.copy(
+              this.templatePath('editorconfig'),
+              this.destinationPath('.editorconfig')
+            );
+            this.fs.copy(
+              this.templatePath('jshintrc'),
+              this.destinationPath('.jshintrc')
+            );
+        }
+    },
+
+    install: function () {
+        this.installDependencies();
+    }
 });
